314ef9d54c87d0c891b3059c4899525b9d3ffc0e
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-31 19:54:59 -0400

    Initialized first endpoint and tested with postman

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Endpoints
+// http://localhost:5005 (BASE URL)
+
+// GET
+// http://localhost:5005/
+app.get("/", (req, res) => {
+    console.log(req.headers)
+    res.send("This is a response!");
+
+})
+
 
 // Starting a Server
 const PORT = process.env.PORT || 5005;
