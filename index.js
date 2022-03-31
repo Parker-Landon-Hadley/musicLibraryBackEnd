e3a1303600ae533e7d166810d0725bd64f80ab60
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-31 19:34:49 -0400

    Added nodemon

---
+++ index.js
@@ -0,0 +1,15 @@
+// Imports
+const express = require("express");
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+
+
+// Starting a Server
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT, () => {
+    console.log(`Server running fast on PORT: ${PORT}`);
+});
