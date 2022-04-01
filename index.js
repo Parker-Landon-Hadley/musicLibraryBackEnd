a5a16992ced4a1ae0ef2e73d0e6493ac1aef9abf
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-31 20:55:40 -0400

    Created and tested POST with postman

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Endpoints
 // http://localhost:5005 (BASE URL)
@@ -13,10 +14,27 @@ app.use(express.json());
 // GET all music
 // http://localhost:5005/api/songs
 app.get("/api/songs", (req, res) => {
-    const songs = repoContext.songs.findAllSongs()
+    const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
 
-})
+});
+
+// Get song by id
+// http://localhost:5005/api/songs/:id
+app.get("/api/songs/:id", (req, res) => {
+    const id = req.params.id;
+    const songsId = repoContext.songs.findSongById(id)
+    return res.send(songsId);
+
+});
+
+// POST new song
+// http://localhost:5005/api/songs
+app.post("/api/songs", (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.status(201).send(addedSong)
+});
 
 
 // Starting a Server
